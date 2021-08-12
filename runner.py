12a1e86bc71995b0ed15c3ce0b2860a00f25b2c7
Ein <erjill87@gmail.com>
2021-08-13 00:39:31 +0800

    License, funding, also cli support for the runner

--- runner.py
+++ runner.py
@@ -1,5 +1,11 @@
 from cmpd import CMPD
+from argparse import ArgumentParser
 
-pack_id = 478526
+parser = ArgumentParser()
+parser.add_argument('pack_id', type=int, help='Project ID of the modpack')
+
+args = parser.parse_args()
+
+pack_id = args.pack_id
 downloader = CMPD(pack_id)
 downloader.download_modpack()
